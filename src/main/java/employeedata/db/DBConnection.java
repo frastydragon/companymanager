715e761cf83567c6dd80813f9a945af2e0c88369
frastydragon <meissen54321@gmail.com>
2025-05-03 04:55:14 -0400

    "update"

--- src/main/java/employeedata/db/DBConnection.java
+++ src/main/java/employeedata/db/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/employeeData"; // Adjust DB name
     private static final String USER = "root";
-    private static final String PASSWORD = "meissen30097";//your own password
+    private static final String PASSWORD = "miata30097";//your own password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
